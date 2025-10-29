4efff4931a683cabbd891da96fb9c9de7f58db01
chickundersea <chickundersea@gmail.com>
2025-10-29 09:35:06 +0800

    add constructor

---
+++ d.js
@@ -0,0 +1,14 @@
+class HeroCreator {
+ constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+  attack(){
+    console.log("AAAAAAA!");
+    
+  }
+}
+
+
+const h1 = new HeroCreator("aa", 20);
+console.log(h1);
